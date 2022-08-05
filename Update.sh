8585c8bb52414c2ce341d56ce769c66f4b5df8ce
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-05 12:17:49 +0930

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,41 @@
+sudo apt update
+
+sudo apt-get update
+
+sudo apt upgrade
+
+sudo apt-get upgrade
+
+sudo apt-get install curl
+
+sudo apt-get install wget
+
+sudo apt-get install git
+
+sudo apt-get install git-all
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-preview.list)"
+
+sudo apt-get update
+
+sudo apt update
+
+sudo apt-get upgrade
+
+sudo apt upgrade
+
+sudo apt-get install -y mssql-server
+
+sudo apt-get install mssql-server -y
+
+sudo /opt/mssql/bin/mssql-conf setup
+
+systemctl status mssql-server --no-pager
+
+sudo apt-get update 
+
+sudo apt update
+
+sudo reboot
