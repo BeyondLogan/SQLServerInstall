7d03491d5751d1ed217a80a3fee02c2dbbf2a874
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-05 12:25:01 +0930

    Create SetUp2.sh

---
+++ SetUp2.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+
+sudo apt update
+
+sudo apt-get upgrade
+
+sudo apt upgrade
+
+sudo apt-get install nano
+
+sudo apt-get install gparted
+
+sudo apt-get install geparted
+
+echo Plz Enter This Command But Ween Entering Change The Your Password To Yours 'sqlcmd -S localhost -U sa -P <YourPassword>'
